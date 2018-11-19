280c3088eafdbbb9f8744df7602811eac367b012
Nick Anstoetz <nanstoetz@lcs.on.ca>
2018-11-19 09:23:02 -0500

    I finished the square and the rectangle

--- Extensions.swift
+++ Extensions.swift
@@ -59,30 +59,30 @@ public extension Tortoise {
     }
 
     //drawing A
-    func uppercaseA() {
+    func uppercaseA(scaleFactor scale: Double = 1.0) {
 
         //make the turtle go up
         self.penUp()
-        self.forward(20)
+        self.forward(20 * scale)
 
         //make a curve to start the A
         self.penDown()
         self.right(180)
-        self.curve(withSize: -15, withSize: 12, drawSides: 8)
+        self.curve(withSize: -15, withSize: 12 * scale, drawSides: 8)
 
         //make turte go up
         self.penDown()
         self.right(20)
-        self.forward(60)
+        self.forward(60 * scale)
 
         //make a circle
         self.right(20)
-        self.curve(withSize: -14, withSize: 15, drawSides: 15)
+        self.curve(withSize: -14, withSize: 15 * scale, drawSides: 15)
 
         //make the top of the A
         self.right(70)
         self.penDown()
-        self.forward(100)
+        self.forward(100 * scale)
 
         //make the turtle go down
         self.penDown()
@@ -90,110 +90,110 @@ public extension Tortoise {
         self.setY(0)
 
         self.right(100)
-        self.forward(115)
+        self.forward(115 * scale)
 
         self.right(30)
-        self.curve(withSize: -7, withSize: 15, drawSides: 7)
+        self.curve(withSize: -7, withSize: 15 * scale, drawSides: 7)
 
         self.penDown()
         self.right(110)
-        self.forward(115)
+        self.forward(115 * scale)
 
         self.penUp()
         self.setY(0)
         self.left(60)
     }
     //start drawing n
-    func LowercaseN() {
+    func LowercaseN(scaleFactor scale: Double = 1.0) {
         self.setY(78)
         self.right(40)
 
-        self.curve(withSize: 10, withSize: 7, drawSides: 3)
+        self.curve(withSize: 10, withSize: 7 * scale, drawSides: 3)
 
         self.penDown()
         self.setY(0)
-        self.setY(75)
+        self.setY(75 * scale)
         self.left(130)
-        self.curve(withSize: 20, withSize: 12, drawSides: 8)
+        self.curve(withSize: 20, withSize: 12 * scale, drawSides: 8)
         self.penDown()
         self.setY(20)
-        self.curve(withSize: -10, withSize: 10, drawSides: 5)
+        self.curve(withSize: -10, withSize: 10 * scale, drawSides: 5)
         self.penUp()
         self.setY(0)
 
     }
     //start drawing s
-    func LowercaseS() {
+    func LowercaseS(scaleFactor scale: Double = 1.0) {
         self.setY(14)
         self.right(20)
         self.penDown()
-        self.forward(75)
+        self.forward(75 * scale)
         self.right(120)
-        self.forward(60)
-        self.curve(withSize: 20, withSize: 8, drawSides: 15)
+        self.forward(60 * scale)
+        self.curve(withSize: 20, withSize: 8 * scale, drawSides: 15)
         self.penDown()
-        self.forward(40)
+        self.forward(40 * scale)
         self.penUp()
         self.setY(0)
         self.left(30)
         }
     //start drawing t
-    func LowercaseT() {
+    func LowercaseT(scaleFactor scale: Double = 1.0) {
 
         self.left(40)
         self.setY(0)
-        self.curve(withSize: -40, withSize: 20, drawSides: 3)
-        self.curve(withSize: -7, withSize: 15, drawSides: 5)
+        self.curve(withSize: -40, withSize: 20 * scale, drawSides: 3)
+        self.curve(withSize: -7, withSize: 15 * scale, drawSides: 5)
         self.penDown()
-        self.forward(60)
+        self.forward(60 * scale)
         self.penUp()
         self.left(80)
         self.setY(0)
     }
     //start drawing O
-    func LowercaseO(){
-        self.setY(53)
+    func LowercaseO(scaleFactor scale: Double = 1.0){
+        self.setY(53 * scale)
         self.right(90)
-        self.curve(withSize: -10, withSize: 10, drawSides: 6)
-        self.curve(withSize: -20, withSize: 15, drawSides: 17)
-        self.curve(withSize: -10, withSize: 10, drawSides: 6)
+        self.curve(withSize: -10, withSize: 10 * scale, drawSides: 6)
+        self.curve(withSize: -20, withSize: 15 * scale, drawSides: 17)
+        self.curve(withSize: -10, withSize: 10 * scale, drawSides: 6)
         self.penDown()
         self.right(20)
-        self.forward(50)
+        self.forward(50 * scale)
         self.penUp()
         self.setY(0)
         self.left(90)
     }
-    func LowercaseE(){
-        self.setY(53)
+    func LowercaseE(scaleFactor scale: Double = 1.0){
+        self.setY(53 * scale)
         self.penDown()
         self.right(80)
-        self.forward(40)
-        self.curve(withSize: -10, withSize: 10, drawSides: 5)
-        self.curve(withSize: -20, withSize: 15, drawSides: 12)
+        self.forward(40 * scale)
+        self.curve(withSize: -10, withSize: 10 * scale, drawSides: 5)
+        self.curve(withSize: -20, withSize: 15 * scale, drawSides: 12)
         self.penDown()
         self.left(17)
-        self.forward(60)
+        self.forward(60 * scale)
         self.penUp()
         self.setY(0)
         self.left(15)
         
     }
-    func LowercaseZ(){
-        self.setY(55)
+    func LowercaseZ(scaleFactor scale: Double = 1.0){
+        self.setY(55 * scale)
         self.right(88)
         self.penDown()
-        self.forward(50)
+        self.forward(50 * scale)
         self.right(135)
-        self.forward(80)
+        self.forward(80 * scale)
         self.left(135)
-        self.forward(70)
+        self.forward(70 * scale)
         self.right(110)
         self.penDown()
-        self.forward(50)
-        self.curve(withSize: 20, withSize: 8, drawSides: 15)
+        self.forward(50 * scale)
+        self.curve(withSize: 20, withSize: 8 * scale, drawSides: 15)
         self.penDown()
-        self.forward(40)
+        self.forward(40 * scale)
         self.penUp()
         self.setY(0)
         self.left(30)
